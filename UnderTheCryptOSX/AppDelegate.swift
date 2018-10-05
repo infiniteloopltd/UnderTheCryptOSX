74c60130e91279d463e5eb0ddd34337ebbeaf532
fiach <fiach.reid@gmail.com>
2018-10-05 21:49:41 +0100

    Initial Commit

---
+++ UnderTheCryptOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnderTheCryptOSX
+//
+//  Created by Fiach Reid on 05/10/2018.
+//  Copyright © 2018 Infinite Loop Development Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UnderTheCryptOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UnderTheCryptOSX
+//
+//  Created by Fiach Reid on 05/10/2018.
+//  Copyright © 2018 Infinite Loop Development Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
