33872037cdc17b5c216248b1cbee9ac73a12089c
fiach <fiach.reid@gmail.com>
2018-10-06 10:58:20 +0100

    Changing Title, adding quit on last window closed.

--- UnderTheCryptOSX/AppDelegate.swift
+++ UnderTheCryptOSX/AppDelegate.swift
@@ -23,6 +23,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool
+    {
+        return true
+    }
+    
 
 }
 
